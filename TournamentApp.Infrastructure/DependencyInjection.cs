1ad2e10da925dc25e80bb2f6fd6dbdc5d02d6e18
Vladislav Chinnov <v.chinnov@sreda.capital>
2023-03-27 08:13:27 +0400

    Добавил возможность использовать токены в swagger

--- TournamentApp.Infrastructure/DependencyInjection.cs
+++ TournamentApp.Infrastructure/DependencyInjection.cs
@@ -11,7 +11,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-
         services.AddDbContext<TournamentAppContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
         services.AddScoped<ITournamentAppContext>(provider => provider.GetRequiredService<TournamentAppContext>());
